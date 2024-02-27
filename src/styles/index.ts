f1933b8029c125853c3b1404061a8ddd8a43ca99
Josh Beard <josh@jlbeard.com>
2024-02-27 16:10:15 -0500

    Referencing bulma from npm

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,5 @@
 import "@fortawesome/fontawesome-free-webfonts";
 import "@fortawesome/fontawesome-free-webfonts/css/fa-brands.css";
 import "@fortawesome/fontawesome-free-webfonts/css/fa-regular.css";
-import "./bulma.min.css";
+import "bulma/css/bulma.min.css";
 import "./site.scss";
